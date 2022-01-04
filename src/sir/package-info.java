71122ac0c10787cfb8f6850f3eb9654686d58d5b
zyf0726 <zyf0726@pku.edu.cn>
2022-01-04 12:47:47 +0800

    SIR [Doubly-Linked-List] [linkedlist]

---
+++ src/sir/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * ======= Software-artifact Infrastructure Repository (SIR) ========
+ * 
+ * https://sir.csc.ncsu.edu/content/sir.php [Doubly-Linked-List] [linkedlist]
+ * 
+ */
+
+package sir;
